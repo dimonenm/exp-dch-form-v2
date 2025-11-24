3a758c4e264394471d3b16369caa4ffb67d42eb2
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-11-24 17:51:12 +0300

    w24.11.2025

---
+++ src/app/auth/db/getData.ts
@@ -0,0 +1,30 @@
+interface IObjectSeizedDuringInspectionOfSceneItem {
+	handprints: string
+	shoe marks: string
+	signs of forced entry
+	traces of fabric
+	tire tracks
+	traces of biological origin
+	other traces
+}
+
+interface IInspectionIncidentSceneWithAndWithoutSpecialistsItem {
+	incidentReportNumber: number
+	departmentOfInternalAffairs: string
+	specialist: string
+	timeOfInspection: string
+	typeOfCrime: string
+	address: string
+	methodOfExecution: string
+	fullNameOfVictim: string
+	amountOfDamage: string
+	CUSP: string
+	numberCriminalCase: string
+	articleOfCriminalCode: string
+	objectsSeizedDuringInspectionOfScene: string
+}
+
+
+export function getInspectionIncidentSceneWithAndWithoutSpecialists() {
+
+}
