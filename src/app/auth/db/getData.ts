974216b3b507adb751120da4f24defe05fe5af0b
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-11-28 17:47:49 +0300

    w28.11.2025

--- src/app/auth/db/getData.ts
+++ src/app/auth/db/getData.ts
@@ -1,15 +1,15 @@
 interface IObjectSeizedDuringInspectionOfSceneItem {
-	handprints: string
-	shoeMarks: string
-	signsOfForcedEntry: string
-	tracesOfFabric: string
-	tireTracks: string
-	tracesOfBiologicalOrigin: string
-	otherTraces: string
+	handprints: string | null
+	shoeMarks: string | null
+	signsOfForcedEntry: string | null
+	tracesOfFabric: string | null
+	tireTracks: string | null
+	tracesOfBiologicalOrigin: string | null
+	otherTraces: string | null
 }
 
 interface IInspectionIncidentSceneIncludedOrNotIncludedInSummary {
-	incidentReportNumber: number
+	incidentReportNumber: number | null
 	departmentOfInternalAffairs: string
 	specialist: string
 	timeOfInspection: string
@@ -36,7 +36,47 @@ interface IFactsOfNotInvolvingSpecialists {
 	fullNameOfPersonCommittedCrime: string
 }
 
+interface INotProvidedFingerprintAndButocard {
+	incidentReportNumber: number
+	fullName: string
+	reasonForVerification: string
+	fingerprint: string
+	butocard: string
+}
 
-export function getInspectionIncidentSceneWithAndWithoutSpecialists() {
+interface INotProvidedMaterials {
+	incidentReportNumber: number
+	fullName: string
+	reasonForVerification: string
+	fingerprint: string
+	butocard: string
+}
 
+
+export function getInspectionIncidentSceneIncludedOrNotIncludedInSummary(): IInspectionIncidentSceneIncludedOrNotIncludedInSummary[] {
+	return [
+		{
+			incidentReportNumber: 2,
+			departmentOfInternalAffairs: 'Феодосия',
+			specialist: 'Алпатов',
+			timeOfInspection: '14:00-15:30',
+			typeOfCrime: 'кража велосипеда',
+			address: 'пгт. Коктебель',
+			methodOfExecution: 'свободный доступ',
+			fullNameOfVictim: 'С .А.П.',
+			amountOfDamage: '11 300 р.',
+			CUSP: '18549 от 18.11.2025',
+			numberCriminalCase: '',
+			articleOfCriminalCode: '',
+			objectsSeizedDuringInspectionOfScene: {
+				handprints: null,
+				shoeMarks: null,
+				signsOfForcedEntry: null,
+				tracesOfFabric: '1 ЛТ',
+				tireTracks: null,
+				tracesOfBiologicalOrigin: null,
+				otherTraces: null,
+			}
+		}
+	]
 }
